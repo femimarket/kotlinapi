34bede8142692454bdb290836fcbbbebfb8a2a9e
femimarket <BJTDhwzXnYx7TLwQdyhVoArwbQnNAMJj2gq5Zs5hpump@femi.market>
2026-06-20 19:08:45 +0100

    update 2026-06-20 19:08:45

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.femimarket"
-version = "4.2.0"
+version = "4.3.0"
 
 mavenPublishing {
     publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
